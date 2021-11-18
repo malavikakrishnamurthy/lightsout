95ebfe3a3a4d6ababc2121060d38cbc5f16a1654
malavikakrishnamurthy <55066733+malavikakrishnamurthy@users.noreply.github.com>
2021-11-18 17:32:11 -0500

    Add files via upload
    Al functionality complete

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ MQTT_CLIENT.connect({ onSuccess: myClientConnected });
 // This function sends a message to the Raspberry Pi to either turn on or off station 1
 function workStation1(button)
 {
+  // If user wanted to turn off Station 1
   if(document.getElementById("workStation1").value=="Turn OFF Station 1"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:OFF:1");
@@ -19,9 +20,9 @@ function workStation1(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off Station 1");
-   document.getElementById("workStation1").value="Turn ON Station 1";}
+    document.getElementById("workStation1").value="Turn ON Station 1";}
 
+  // If user wanted to turn on Station 1
   else if(document.getElementById("workStation1").value=="Turn ON Station 1"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:ON:1");
@@ -31,13 +32,13 @@ function workStation1(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Station 1");
-   document.getElementById("workStation1").value="Turn OFF Station 1";}
+    document.getElementById("workStation1").value="Turn OFF Station 1";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off station 2
 function workStation2(button)
 {
+  // If user wanted to turn off Station 2
   if(document.getElementById("workStation2").value=="Turn OFF Station 2"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:OFF:2");
@@ -47,9 +48,9 @@ function workStation2(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off Station 2");
-   document.getElementById("workStation2").value="Turn ON Station 2";}
+    document.getElementById("workStation2").value="Turn ON Station 2";}
 
+  // If user wanted to turn on Station 2
   else if(document.getElementById("workStation2").value=="Turn ON Station 2"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:ON:2");
@@ -59,13 +60,13 @@ function workStation2(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Station 2");
-   document.getElementById("workStation2").value="Turn OFF Station 2";}
+    document.getElementById("workStation2").value="Turn OFF Station 2";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off station 3
 function workStation3(button)
 {
+  // If user wanted to turn off Station 3
   if(document.getElementById("workStation3").value=="Turn OFF Station 3"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:OFF:3");
@@ -75,11 +76,10 @@ function workStation3(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off Station 3");
-   document.getElementById("workStation3").value="Turn ON Station 3";}
+    document.getElementById("workStation3").value="Turn ON Station 3";}
 
+  // If user wanted to turn on Station 3
   else if(document.getElementById("workStation3").value=="Turn ON Station 3"){
-    
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:ON:3");
   
@@ -88,13 +88,13 @@ function workStation3(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Station 3");
    document.getElementById("workStation3").value="Turn OFF Station 3";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off station 4
 function workStation4(button)
 {
+  // If user wanted to turn off Station 4
   if(document.getElementById("workStation4").value=="Turn OFF Station 4"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:OFF:4");
@@ -104,9 +104,9 @@ function workStation4(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off Station 4");
-   document.getElementById("workStation4").value="Turn ON Station 4";}
+    document.getElementById("workStation4").value="Turn ON Station 4";}
 
+  // If user wanted to turn on Station 4
   else if(document.getElementById("workStation4").value=="Turn ON Station 4"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:ON:4");
@@ -116,13 +116,13 @@ function workStation4(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Station 4");
    document.getElementById("workStation4").value="Turn OFF Station 4";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off all lights
 function allStations(button)
 {
+  // If user wanted to turn off all stations
   if(document.getElementById("workStation5").value=="Turn OFF All Stations"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:OFF:5");
@@ -132,9 +132,9 @@ function allStations(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off All Stations");
-   document.getElementById("workStation5").value="Turn ON All Stations";}
+    document.getElementById("workStation5").value="Turn ON All Stations";}
 
+  // If user wanted to turn on all stations
   else if(document.getElementById("workStation5").value=="Turn ON All Stations"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("STATION:ON:5");
@@ -144,13 +144,13 @@ function allStations(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On All Stations");
-   document.getElementById("workStation5").value="Turn OFF All Stations";}
+    document.getElementById("workStation5").value="Turn OFF All Stations";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off pir sensors
 function pirs(button)
 {
+  // If user wanted to turn off motion sensors
   if(document.getElementById("pirs").value=="Turn OFF Motion Sensors"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("PIRS:OFF");
@@ -160,9 +160,9 @@ function pirs(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off Motion Sensors");
-   document.getElementById("pirs").value="Turn ON Motion Sensors";}
+    document.getElementById("pirs").value="Turn ON Motion Sensors";}
 
+  // If user wanted to turn on motion sensors
   else if(document.getElementById("pirs").value=="Turn ON Motion Sensors"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("PIRS:ON");
@@ -172,13 +172,13 @@ function pirs(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Motion Sensors");
-   document.getElementById("pirs").value="Turn OFF Motion Sensors";}
+    document.getElementById("pirs").value="Turn OFF Motion Sensors";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off microphones
 function mics(button)
 {
+  // If user wanted to turn off microphones
   if(document.getElementById("mics").value=="Turn OFF Microphones"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("MICS:OFF");
@@ -188,9 +188,9 @@ function mics(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn Off Microphones");
-   document.getElementById("mics").value="Turn ON Microphones";}
+    document.getElementById("mics").value="Turn ON Microphones";}
 
+  // If user wanted to turn on microphones
   else if(document.getElementById("mics").value=="Turn ON Microphones"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("MICS:ON");
@@ -200,13 +200,13 @@ function mics(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Microphones");
-   document.getElementById("mics").value="Turn OFF Microphones";}
+    document.getElementById("mics").value="Turn OFF Microphones";}
 }
 
 // This function sends a message to the Raspberry Pi to either turn on or off automatic weighting
 function weightControl(button)
 {
+  // If user wanted to control weights manually
   if(document.getElementById("weightControl").value=="Switch to Manual Weighting"){
     // create a new MQTT message with a specific payload
     PIRweight = document.getElementById("weighting").value;
@@ -221,9 +221,9 @@ function weightControl(button)
 
     // Update Weight Setting Info
     document.getElementById('textInput').value=PIRweight+"% PIRS "+MICweight+"% MICS"; 
-    alert("Published Turn On Manual Weighting");
     document.getElementById("weightControl").value="Revert to Automatic Weighting";}
 
+  // If user wanted to revert to automatic weighting
   else if(document.getElementById("weightControl").value=="Revert to Automatic Weighting"){
     // create a new MQTT message with a specific payload
     var mqttMessage = new Paho.MQTT.Message("WEIGHT:OFF");
@@ -233,14 +233,13 @@ function weightControl(button)
   
     // Publish the message
     MQTT_CLIENT.send(mqttMessage);
-    alert("Published Turn On Automatic Weighting");
-   document.getElementById("weightControl").value="Switch to Manual Weighting";}
+    document.getElementById("weightControl").value="Switch to Manual Weighting";}
 }
 
+// Update Text Input called on change for slider object on html, updates textArea with current
+// value when slider bar changes
 function updateTextInput(val) {
-
   var micWeight = 100 - val
-
   var mqttMessage = new Paho.MQTT.Message("WEIGHT:ON:"+val+":"+micWeight);
   
   // Set the topic it should be published to
@@ -248,7 +247,6 @@ function updateTextInput(val) {
   
   // Publish the message
   MQTT_CLIENT.send(mqttMessage);
-  alert("Published Manual Weighting Setting");
 
   // Update Weight Setting Info
   document.getElementById('textInput').value=val+"% PIRS "+micWeight+"% MICS"; 
@@ -256,39 +254,35 @@ function updateTextInput(val) {
 
 // This is the function which handles subscribing to topics after a connection is made
 function myClientConnected() {
-    alert("In connect function");
     // Subscribe to whichever topic you are reading from
     MQTT_CLIENT.subscribe("pir/data/baseStation");
-    alert("Subscribed to topic");
+    alert("Connected");
 }
 
 // This is the function which handles received messages
 function myMessageArrived(message) {
-
-  alert("New message arrived");
   
-  
-    // Parse and case actions on incoming messages
-    var messageString = String(message.payloadString);
-    const myArray = messageString.split(":");
-    alert(myArray[0]);
-    // Weight Setting (edit like in updateTextInputFunction)
-    if (myArray[0] == "WEIGHT"){
-      alert("New message arrived: WEIGHT")
-      document.getElementById('textInput').value=myArray[1]+"% PIRS "+myArray[2]+"% MICS"; 
-    }
+  // Parse and case actions on incoming messages
+  var messageString = String(message.payloadString);
+  const myArray = messageString.split(":");
+    
+  // Weight Setting
+  if (myArray[0] == "WEIGHT"){
+    alert("New message arrived: WEIGHT")
+    document.getElementById('textInput').value=myArray[1]+"% PIRS "+myArray[2]+"% MICS"; 
+  }
 
-    // Station On/Off
+  // Station On/Off
     
 
-    // Get the payload
-    var messageBody = message.payloadString;
+  // Get the payload
+  var messageBody = message.payloadString;
   
-    // Create a new HTML element wrapping the message payload
-    var messageHTML = $("<p>"+messageBody+"</p>");
+  // Create a new HTML element wrapping the message payload
+  var messageHTML = $("<p>"+messageBody+"</p>");
   
-    // Insert it inside the ```id=updateMe``` element above everything else that is there 
-    $("#updateMe").prepend(messageHTML);
+  // Insert it inside the ```id=updateMe``` element above everything else that is there 
+  $("#updateMe").prepend(messageHTML);
   };
   
   // Tell MQTT_CLIENT to call myMessageArrived(message) each time a new message arrives
